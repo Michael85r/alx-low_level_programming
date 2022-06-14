eb64ff3027772a1da6ecc9d0388d31b582d632e6
Michael85r <nduxy13@gmail.com>
2022-06-14 19:49:04 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ *_strlen -length of a string
+ *
+ *Rturn: void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+int u;
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
